e531417f66e7088125e516221f2e51164352fe41
nalutm <nayara.luiza21@gmail.com>
2022-08-29 20:25:58 -0300

    :spearkles: feat: Show password option on login page

--- assets/js/controllers/login-controller.js
+++ assets/js/controllers/login-controller.js
@@ -1,5 +1,6 @@
 import { clientService } from "../service/client-service.js";
 import { validate } from "../login-validation.js";
+import { showPassword } from "../show-password.js";
 
 const inputs = document.querySelectorAll('[data-input]');
 inputs.forEach(input => input.addEventListener('focusout', (e) => validate(e.target)));
@@ -8,6 +9,12 @@ const submitBtn = document.querySelector('[data-form="submit"]');
 submitBtn.addEventListener('click', (e) => {
   validateLogin(e);
 });
+
+const showPasswordIcon = document.querySelector('[data-password="icon"]');
+
+showPasswordIcon.addEventListener('click', (e) => {
+  showPassword(e);
+})
 
 async function validateLogin(e) {
   e.preventDefault();
