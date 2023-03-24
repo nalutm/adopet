b29a1a9f509d9a0101f8f48dc9bdd4ab95e23667
Nayara Luiza Tavares Moraes <nluiza@ibm.com>
2023-03-23 23:11:29 -0300

    :goal_net: fix: Changed API address

--- assets/js/service/client-service.js
+++ assets/js/service/client-service.js
@@ -1,48 +1,44 @@
 const newProfile = (email, name, password) => {
-  return fetch('http://localhost:3000/profile', {
+  return fetch('https://adopet-api.herokuapp.com/profile', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       email: email,
       name: name,
-      password: password
-    })
-  })
-  .then(r => {
+      password: password,
+    }),
+  }).then((r) => {
     if (r.ok) return r.body;
-    throw new Error('Não foi possível criar o perfil.')
+    throw new Error('Não foi possível criar o perfil.');
   });
-}
+};
 
 const validateNewProfile = () => {
-  return fetch('http://localhost:3000/profile')
-  .then(r => {
+  return fetch('https://adopet-api.herokuapp.com/profile').then((r) => {
     if (r.ok) return r.json();
-    throw new Error('Não foi possível realizar o cadastro.')
+    throw new Error('Não foi possível realizar o cadastro.');
   });
-}
+};
 
 const validateLogin = () => {
-  return fetch('http://localhost:3000/profile')
-  .then(r => {
+  return fetch('https://adopet-api.herokuapp.com/profile').then((r) => {
     if (r.ok) return r.json();
-    throw new Error('Não foi possível realizar o login.')
+    throw new Error('Não foi possível realizar o login.');
   });
-}
+};
 
 const animalList = () => {
-  return fetch(`http://localhost:3000/animal`)
-  .then(r => {
+  return fetch(`https://adopet-api.herokuapp.com/animal`).then((r) => {
     if (r.ok) return r.json();
-    throw new Error('Não foi possível listar os animais.')
+    throw new Error('Não foi possível listar os animais.');
   });
-}
+};
 
 export const clientService = {
   newProfile,
   validateLogin,
   animalList,
-  validateNewProfile
-}
+  validateNewProfile,
+};
